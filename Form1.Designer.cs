ed7d6c67d5979c822798f1d746f29393171eb08b
castroy092003@outlook.com <castroy092003@outlook.com>
2024-03-12 23:38:37 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace BUDGY_2._0
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginPage));
+            panel1 = new Panel();
+            linkLabel1 = new LinkLabel();
+            cbxShow = new CheckBox();
+            btnLogin = new Button();
+            tbxPassword = new TextBox();
+            tbxUsername = new TextBox();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.Controls.Add(linkLabel1);
+            panel1.Controls.Add(cbxShow);
+            panel1.Controls.Add(btnLogin);
+            panel1.Controls.Add(tbxPassword);
+            panel1.Controls.Add(tbxUsername);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(557, 179);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(274, 282);
+            panel1.TabIndex = 0;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(81, 242);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(118, 20);
+            linkLabel1.TabIndex = 16;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "No Account Yet?";
+            // 
+            // cbxShow
+            // 
+            cbxShow.AutoSize = true;
+            cbxShow.Font = new Font("Segoe UI", 7.20000029F, FontStyle.Regular, GraphicsUnit.Point);
+            cbxShow.Location = new Point(22, 180);
+            cbxShow.Name = "cbxShow";
+            cbxShow.Size = new Size(122, 21);
+            cbxShow.TabIndex = 15;
+            cbxShow.Text = "Show password";
+            cbxShow.UseVisualStyleBackColor = true;
+            cbxShow.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(93, 207);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 14;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // tbxPassword
+            // 
+            tbxPassword.Location = new Point(19, 147);
+            tbxPassword.Name = "tbxPassword";
+            tbxPassword.PasswordChar = '●';
+            tbxPassword.Size = new Size(229, 27);
+            tbxPassword.TabIndex = 13;
+            // 
+            // tbxUsername
+            // 
+            tbxUsername.Location = new Point(19, 66);
+            tbxUsername.Name = "tbxUsername";
+            tbxUsername.Size = new Size(229, 27);
+            tbxUsername.TabIndex = 12;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.FromArgb(0, 0, 0, 46);
+            pictureBox2.BackgroundImage = (Image)resources.GetObject("pictureBox2.BackgroundImage");
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(19, 103);
+            pictureBox2.Margin = new Padding(3, 2, 3, 2);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(32, 39);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 11;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(22, 31);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(24, 30);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(46, 117);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Password";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(46, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Username";
+            // 
+            // LoginPage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(864, 638);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Name = "LoginPage";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label1;
+        private LinkLabel linkLabel1;
+        private CheckBox cbxShow;
+        private Button btnLogin;
+        private TextBox tbxPassword;
+        private TextBox tbxUsername;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,19 @@
+﻿namespace BUDGY_2._0
+{
+    public partial class LoginPage : Form
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //so that the user can see the password
+            if (cbxShow.Checked)
+                tbxPassword.PasswordChar = '\0';//null
+            else
+                tbxPassword.PasswordChar = '●';
+        }
+    }
+}
